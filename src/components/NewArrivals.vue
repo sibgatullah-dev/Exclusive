7476ed51f530429198269b590a2ec8a56caff51b
Sibgatullah <sibgatullah881@gmail.com>
2025-03-26 22:47:37 +0600

    new arraivel card

--- src/components/NewArrivals.vue
+++ src/components/NewArrivals.vue
@@ -6,7 +6,21 @@
             </div>
             <div class="flex">
                 <div class="w-1/4">
-                    
+                    <div class="flex bg-[#F5F5F5] py-[34px] items-center justify-center">
+                        <img src="../assets/images/p4.png" alt="">
+                    </div>
+                    <div class="short_detail">
+                        <h3 class="text-[16px] font-Poppins text-black font-medium leading-6 pb-2 pt-4">ASUS FHD Gaming Laptop</h3>
+                        <p class="font-Poppins font-medium text-[16px] leading-6"><span class="text-red-400">$700</span><del class="text-[rgba(0,0,0,0.5)] pl-3">$820</del></p>
+                    </div>
+                    <div class="flex gap-x-3 font-Poppins">
+                        <div class="star">
+                            <i v-for="star in 5" class="fa-solid fa-star text-[#FFAD33]"></i>
+                        </div>
+                        <div class="rate">
+                            <span class="text-[rgba(0,0,0,0.5)]">(88)</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
